614dc2377ff3782003df1ea9365e07b10ddd1aa6
jkderrick028 <jkderrick028@gmail.com>
2024-08-30 15:47:41 -0400

    rescale structural maps

--- analysis/START_R3_feature_maps.m
+++ analysis/START_R3_feature_maps.m
@@ -5,8 +5,9 @@ function START_R3_feature_maps
 % 1. selectivity to 9 target locations
 % 2. selectivity to 3 trial epochs (cue, delay, response)
 % 3. selectivity to 9 target during cue, delay, response respectively
+% 4. selectivity to 9 target x 3 epochs
 % 
-% last modified: 2024.08.13
+% last modified: 2024.08.26
 
 import spikes.*;
 import utils_dx.*;
@@ -22,12 +23,6 @@ if ~exist(resultsPath, 'dir'), mkdir(resultsPath); end
 
 subjectStr                          = 'Buzz';
 arrayStr                            = 'NSP1';
-sessionStr                          = '20171201'; 
-taskStr                             = 'KM'; 
-
-resultsPath_taskTuning              = fullfile(projectPath, 'results', 'spikeTuningVectors', sprintf('START_B1_extractSignal_%s', taskStr));
-MAT_tuningResults                   = fullfile(resultsPath_taskTuning, sprintf('START_B1_extractSignal_%s_%s_channels_results.mat', taskStr, subjectStr));    
-spikeTuningResults                  = load(MAT_tuningResults).spikeTuningResults.(arrayStr).(sprintf('sess_%s', sessionStr)).nineLocations;
 
 output                              = [];
 MAT_output                          = fullfile(resultsPath, sprintf('%s.mat', currfilename)); 
@@ -40,21 +35,40 @@ if exist(PS_maps, 'file')
     system(['rm ' PS_maps]); 
 end
 
+nRows                               = 10;
+nCols                               = 10; 
+nChannelsTotal                      = nRows * nCols; 
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% feature maps KM
+taskStr                             = 'KM'; 
+sessionStr                          = '20171201'; 
+resultsPath_taskTuning              = fullfile(projectPath, 'results', 'spikeTuningVectors', sprintf('START_B1_extractSignal_%s', taskStr));
+MAT_tuningResults                   = fullfile(resultsPath_taskTuning, sprintf('START_B1_extractSignal_%s_%s_channels_results.mat', taskStr, subjectStr));    
+spikeTuningResults                  = load(MAT_tuningResults).spikeTuningResults.(arrayStr).(sprintf('sess_%s', sessionStr)).nineLocations;
+
 chanLinearInds                      = spikeTuningResults.chanLinearInds; 
 conditionInfo                       = spikeTuningResults.conditionInfo; 
 uniqueConditions                    = spikeTuningResults.uniqueConditions; 
 tuningProfile                       = spikeTuningResults.tuningProfile; 
-mcTuningProfile                     = spikeTuningResults.mcTuningProfile; 
-
-nRows                               = 10;
-nCols                               = 10; 
-nChannelsTotal                      = nRows * nCols; 
 excluded_chanLinearInds             = setdiff(1:nChannelsTotal, chanLinearInds); 
 
 target_colors                       = {
     [107, 207, 54]/255, [50, 168, 109]/255, [38, 140, 67]/255;
     [89, 229, 242]/255, [76, 146, 184]/255, [11, 67, 122]/255;
     [227, 232, 88]/255, [232, 179, 88]/255, [186, 89, 74]/255
+};
+
+target_by_epoch_colors              = {
+    [157, 124, 204]/255, [165, 86, 204]/255, [151, 10, 204]/255, ...
+    [204, 176, 175]/255, [204, 102, 100]/255, [204, 9, 23]/255, ...
+    [204, 149, 167]/255, [204, 159, 191]/255, [204, 77, 141]/255, ...
+    [109, 219, 210]/255, [123, 204, 198]/255, [12, 187, 199]/255, ...
+    [89, 229, 242]/255, [66, 138, 204]/255, [6, 90, 204]/255, ...
+    [155, 180, 204]/255, [4, 117, 232]/255, [9, 33, 242]/255, ....
+    [107, 207, 54]/255, [50, 168, 109]/255, [38, 140, 67]/255, ...
+    [140, 204, 182]/255, [74, 200, 113]/255, [14, 180, 55]/255, ...
+    [227, 232, 88]/255, [232, 179, 88]/255, [204, 103, 10]/255
 }; 
 
 % map 1, selectivity to 9 targets
@@ -87,7 +101,8 @@ title('selectivity to targets');
 
 % map 2, selectivity to 3 trial epochs
 trial_epoch_colors = {
-   [207, 75, 54]/255, [38, 49, 140]/255, [168, 50, 156]/255 
+   % [207, 75, 54]/255, [38, 49, 140]/255, [168, 50, 156]/255 
+   [207, 75, 54]/255, [38, 49, 140]/255, [38, 140, 67]/255 
 }; 
 spikes_map2                         = squeeze(sum(tuningProfile, 3)); % shape of 100 x 3
 [M, I]                              = max(spikes_map2, [], 2); 
@@ -191,7 +206,138 @@ xlabel('channels');
 ylabel('channels'); 
 title('selectivity to targets during response'); 
 
-pageHeading = 'feature maps'; 
+
+% map 6, selectivity to 9 target locations x 3 trial epochs
+spikes_map6                         = permute(tuningProfile, [1, 3, 2]); 
+spikes_map6                         = reshape(spikes_map6, nChannelsTotal, []); 
+[M, I]                              = max(spikes_map6, [], 2); 
+feature_map6 = ones(nChannelsTotal, 3);
+
+currCondI = 1;
+for i = 1:27
+    chanInds = find(I == currCondI);
+    feature_map6(chanInds, :) = repmat(target_by_epoch_colors{i}, numel(chanInds), 1);
+    currCondI = currCondI + 1; 
+end % i
+feature_map6(excluded_chanLinearInds, :) = 1;
+feature_map6 = reshape(feature_map6, 10, 10, 3); 
+
+subplot(nHors, nVers, 6);
+imagesc(feature_map6); 
+axis square;
+set(gca, 'YDir', 'normal'); 
+xlabel('channels');
+ylabel('channels'); 
+title('selectivity to targets by epochs'); 
+
+
+pageHeading                         = 'feature maps KM'; 
+addHeadingAndPrint(pageHeading, PS_maps, figI_maps); 
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% feature maps ODR
+
+taskStr                             = 'ODR'; 
+sessionStr                          = '20180307'; 
+resultsPath_taskTuning              = fullfile(projectPath, 'results', 'spikeTuningVectors', sprintf('START_B1_extractSignal_%s', taskStr));
+MAT_tuningResults                   = fullfile(resultsPath_taskTuning, sprintf('START_B1_extractSignal_%s_%s_channels_results.mat', taskStr, subjectStr));    
+spikeTuningResults                  = load(MAT_tuningResults).spikeTuningResults.(arrayStr).(sprintf('sess_%s', sessionStr)).quadrants;
+
+chanLinearInds                      = spikeTuningResults.chanLinearInds; 
+conditionInfo                       = spikeTuningResults.conditionInfo; 
+uniqueConditions                    = spikeTuningResults.uniqueConditions; 
+tuningProfile                       = spikeTuningResults.tuningProfile; 
+excluded_chanLinearInds             = setdiff(1:nChannelsTotal, chanLinearInds); 
+
+target_colors                       = {
+    [89, 229, 242]/255, [107, 207, 54]/255; 
+    [50, 168, 109]/255, [76, 146, 184]/255
+};
+
+target_by_epoch_colors              = {
+    [204, 102, 100]/255, [204, 176, 175]/255, [165, 86, 204]/255, [204, 9, 23]/255, ...
+    [66, 138, 204]/255, [123, 204, 198]/255, [89, 229, 242]/255, [6, 90, 204]/255, ...
+    [140, 204, 182]/255, [107, 207, 54]/255, [50, 168, 109]/255, [38, 140, 67]/255
+}; 
+
+% map 1, selectivity to 4 quadrants
+spikes_map1                         = squeeze(sum(tuningProfile, 2)); % shape of 100 x 4
+[M, I]                              = max(spikes_map1, [], 2); 
+feature_map1                        = ones(nChannelsTotal, 3); 
+
+currCondI = 1;
+for i=1:2
+    for j=1:2
+        chanInds                    = find(I == currCondI);
+        feature_map1(chanInds, :)   = repmat(target_colors{i, j}, numel(chanInds), 1); 
+
+        currCondI                   = currCondI + 1;    
+    end % j
+end % i
+
+feature_map1(excluded_chanLinearInds, :) = 1; 
+feature_map1                        = reshape(feature_map1, 10, 10, 3); 
+
+figure(figI_maps); clf(figI_maps);
+
+subplot(nHors, nVers, 1);
+imagesc(feature_map1); 
+axis square;
+set(gca, 'YDir', 'normal'); 
+xlabel('channels');
+ylabel('channels'); 
+title('selectivity to quadrants'); 
+
+
+% map 2, selectivity to 3 trial epochs
+spikes_map2                         = squeeze(sum(tuningProfile, 3)); % shape of 100 x 3
+[M, I]                              = max(spikes_map2, [], 2); 
+feature_map2                        = ones(nChannelsTotal, 3); 
+
+n_epochs = 3; 
+
+for i = 1:n_epochs
+    chanInds                        = find(I == i); 
+    feature_map2(chanInds, :)       = repmat(trial_epoch_colors{i}, numel(chanInds), 1); 
+end % i
+feature_map2(excluded_chanLinearInds, :) = 1; 
+feature_map2 = reshape(feature_map2, 10, 10, 3);
+
+subplot(nHors, nVers, 2);
+imagesc(feature_map2);
+axis square;
+set(gca, 'YDir', 'normal');
+xlabel('channels');
+ylabel('channels'); 
+title('selectivity to trial epochs'); 
+
+
+% map 3, selectivity to 4 quadrants x 3 trial epochs
+spikes_map3                         = permute(tuningProfile, [1, 3, 2]); 
+spikes_map3                         = reshape(spikes_map3, nChannelsTotal, []); 
+[M, I]                              = max(spikes_map3, [], 2); 
+feature_map3 = ones(nChannelsTotal, 3);
+
+currCondI = 1;
+for i = 1:12
+    chanInds = find(I == currCondI);
+    feature_map3(chanInds, :) = repmat(target_by_epoch_colors{i}, numel(chanInds), 1);
+    currCondI = currCondI + 1; 
+end % i
+feature_map3(excluded_chanLinearInds, :) = 1;
+feature_map3 = reshape(feature_map3, 10, 10, 3); 
+
+subplot(nHors, nVers, 3);
+imagesc(feature_map3); 
+axis square;
+set(gca, 'YDir', 'normal'); 
+xlabel('channels');
+ylabel('channels'); 
+title('selectivity to quadrants by epochs'); 
+
+
+pageHeading                         = 'feature maps ODR'; 
 addHeadingAndPrint(pageHeading, PS_maps, figI_maps); 
 
 save(MAT_output, 'output', '-v7.3');
---
+++ analysis/START_R6_rescale_structural_maps.m
@@ -0,0 +1,119 @@
+function START_R6_rescale_structural_maps
+% this function rescales the structural map from Goldman-Rakic 1984 and
+% runs spatial ACF analysis on the rescaled map. 
+% 
+% last modified: 2024.08.30
+
+import moranI.donutACF_channels; 
+import utils_dx.*; 
+
+close all;
+
+projectPath                         = setProjectPath();
+[currPath, currfilename, currext]   = fileparts(mfilename('fullpath'));
+ANALYSIS                            = 'goldman_rakic';
+resultsPath                         = fullfile(projectPath, 'results', ANALYSIS, currfilename);
+if ~exist(resultsPath, 'dir'), mkdir(resultsPath); end
+
+PS_simulations                      = fullfile(resultsPath, sprintf('simulations.ps'));
+if exist(PS_simulations, 'file')
+    system(['rm ' PS_simulations]);
+end
+
+output                              = [];
+MAT_output                          = fullfile(resultsPath, sprintf('output.mat')); 
+
+image_dir                           = fullfile(projectPath, 'results', ANALYSIS, 'generated_dataset'); 
+image_files                         = {dir(image_dir).name};
+image_files                         = setdiff(image_files, {'.', '..', '.DS_Store'}); 
+
+n_images                            = numel(image_files); 
+
+figI_1                              = 10;
+figI_2                              = 11; 
+monitor                             = 1; 
+
+moranIs                             = cell(n_images, 1); 
+figI_moranI                         = 13; 
+
+for imageI = 1:n_images
+    image                           = imread(fullfile(image_dir, image_files{imageI})); 
+    
+    image                           = rgb2gray(image);
+    
+    image_binary                    = ones(size(image));
+    
+    if monitor
+        figure(figI_1); 
+        clf(figI_1); 
+        imagesc(image_binary, [0, 1]);
+        colormap('gray');
+        colorbar;
+        axis square; 
+    end
+    
+    image_binary(image ==0)         = 0;
+    if monitor
+        imagesc(image_binary, [0, 1]);
+        colormap('gray');
+        colorbar;
+        axis square; 
+    end
+    
+    sampled                         = ones(10, 10);
+    stride                          = ceil(size(image_binary, 1) / 10); 
+    starting_points                 = 1:stride:size(image_binary, 1); 
+    end_points                      = min(starting_points + stride -1, size(image_binary, 1));
+    
+    n_bins                          = 10;
+    for i = 1:n_bins
+        for j = 1:n_bins 
+            sampled(i, j)           = mean(image_binary(starting_points(i):end_points(i), starting_points(j):end_points(j)), 'all');
+        end
+    end
+    
+    if monitor
+        figure(figI_2); 
+        clf(figI_2); 
+        imagesc(sampled, [0, 1]); 
+        colormap('gray');
+        colorbar;
+        axis square;
+    end
+    figure(figI_moranI); 
+    clf(figI_moranI); 
+    moranIs{imageI}                 = donutACF_channels(reshape(sampled, [], 1), []);
+end % imageI
+
+I_reals                             = [];
+
+for imageI = 1:n_images
+    I_real                          = [1, moranIs{imageI}.I_real]; 
+    I_reals                         = [I_reals; I_real]; 
+end % imageI 
+
+distances                           = [0, moranIs{1}(1).uniqueDists]; 
+figI_summary                        = 12;
+figure(figI_summary); clf(figI_summary);
+subplot(2, 1, 1); 
+hold on;
+yline(0, 'LineStyle', '--', 'Color', str2rgb('dark_grey'), 'LineWidth', 1.2)
+plot(distances, I_reals', 'Color', str2rgb('light_grey'), 'LineWidth', 0.7); 
+plot(distances, mean(I_reals, 1), 'Color', 'k', 'LineWidth', 2); 
+xlabel('distances (mm)');
+ylabel('ACF');
+ylim([-0.4, 1]);
+xlim([0, 4]); 
+xticks(0:1:4); 
+title('goldman-rakic ACF');
+box('off');
+
+pageHeadings                        = sprintf('%d simulations', n_images);
+addHeadingAndPrint(pageHeadings, PS_simulations, figI_summary);
+
+output.moranIs                      = moranIs;
+save(MAT_output, 'output', '-v7.3'); 
+
+close all;
+
+end % START_R6_rescale_structural_maps
