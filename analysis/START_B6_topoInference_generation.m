6b68f42f966daa95d0d971ac4f55aff5009be9f1
jkderrick028 <jkderrick028@gmail.com>
2024-12-19 15:53:06 -0500

    update

--- analysis/START_B6_topoInference_generation.m
+++ analysis/START_B6_topoInference_generation.m
@@ -26,7 +26,13 @@ subjectStrs                     = {'Buzz', 'Theo'};
 taskStrs                        = {'ODR', 'KM', 'AL'}; 
 arrayStrs                       = {'NSP0', 'NSP1'}; 
 signal_types                    = {'whole', 'task', 'residuals'}; 
-excludedSessionStrs             = get_excludedSessionStrs(); 
+excludedSessionStrs             = get_excludedSessionStrs();
+
+%% control analysis, removing 4 sessions for AL
+excludedSessionStrs             = [excludedSessionStrs, {'20171109', '20171110', '20171111', '20171112'}]; 
+
+%%
+
 figI                            = 10; 
 nHors                           = 2; 
 nVers                           = 2; 
